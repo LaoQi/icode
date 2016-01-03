b3280281aa998f44ee10ae7c22764d01cfe5605a
LaoQi <qjqjqzy@gmail.com>
2016-01-03 21:02:28 +0800

    csocket

---
+++ csocket/server.c
@@ -0,0 +1,77 @@
+﻿#include "server.h"
+
+HSocket bind_socket(u_short port) {
+
+    INITIALIZE_SOCKET_ENV;
+    //sockfd
+    HSocket server_sockfd = socket(AF_INET,SOCK_STREAM, SOC_PROTOCOL);
+
+    //sockaddr_in
+    struct sockaddr_in server_sockaddr;
+    server_sockaddr.sin_family = AF_INET;
+    server_sockaddr.sin_port = htons(port);
+    server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    ///bind，，success return 0，error return -1
+    if(bind(server_sockfd,(struct sockaddr *)&server_sockaddr,sizeof(server_sockaddr)) == -1) {
+        SOCPERROR("bind");
+        exit(1);
+    }
+
+    //listen，success return 0，error return -1
+    if(listen(server_sockfd, SOCKET_BACKLOG) == -1) {
+        SOCPERROR("listen");
+        exit(2);
+    }
+
+    return server_sockfd;
+}
+
+void main_loop(HSocket server_sockfd) {
+    //client
+    char buffer[BUFFER_SIZE];
+
+    while(TRUE) {
+        struct sockaddr_in client_addr;
+        socklen_t length = sizeof(client_addr);
+
+        HSocket conn = accept(server_sockfd, (struct sockaddr*)&client_addr, &length);
+        IN_ADDR ip_addr = client_addr.sin_addr;
+        LOG("#%d.%d.%d.%d \n", ip_addr.S_un.S_un_b.s_b1, ip_addr.S_un.S_un_b.s_b2, ip_addr.S_un.S_un_b.s_b3, ip_addr.S_un.S_un_b.s_b4);
+        if(conn<0) {
+            SOCPERROR("connect");
+            continue;
+        }
+        memset(buffer,0,sizeof(buffer));
+        int len = recv(conn, buffer, sizeof(buffer),0);
+
+        char html[sizeof(HTML)];
+        strcpy(html, HTML);
+        send(conn, html, strlen(html), 0);
+        CLOSE_SOCKET(conn);
+    }
+}
+
+int main(int argc, char* argv[]) {
+
+    int port = DEFAULT_PORT;
+    if (argc >= 2) {
+        if (strcmp(argv[1], "-p") != 0 || argc < 3) {
+            fputs("Usage: -p port\n\tdefault port is 8888", stdout);
+            exit(1);
+        } else {
+            port = atoi(argv[2]);
+        }
+    }
+
+    HSocket server_sockfd = bind_socket(port);
+    if (server_sockfd < 0) {
+        SOCPERROR("connect error");
+        exit(3);
+    }
+    LOG("Server start at %d\n", port);
+    main_loop(server_sockfd);
+
+    CLOSE_SOCKET(server_sockfd);
+    return 0;
+}
---
+++ csocket/server.h
@@ -0,0 +1,61 @@
+﻿#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#ifdef WIN32
+#define PLATFORM_WIN 1
+#else
+#define PLATFORM_LINUX 1
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef PLATFORM_LINUX
+#include <netinet/in.h>
+#include <unistd.h>
+typedef int HSocket;
+
+#define SOCPERROR(errstr) perror(errstr)
+#define SOC_PROTOCOL 0
+#define ISSOCKHANDLE(x) (x>0)
+#define CLOSE_SOCKET(x) close(x)
+#define INITIALIZE_SOCKET_ENV do {}while(0)
+
+#define LOG(...) do{ char log_buff[1024]; sprintf(log_buff, __VA_ARGS__); fputs(log_buff, stdout); }while(0)
+#endif
+
+#ifdef PLATFORM_WIN
+#pragma comment(lib,"ws2_32.lib")
+#include <WinSock2.h>
+#include <Windows.h>
+typedef SOCKET HSocket;
+typedef int socklen_t;
+
+#define SOCPERROR(errstr) printf("Socket Error : %d\n", WSAGetLastError());//perror(errstr)
+#define SOC_PROTOCOL IPPROTO_TCP
+#define ISSOCKHANDLE(x) (x!=INVALID_SOCKET)
+#define CLOSE_SOCKET(x) closesocket(x)
+#define INITIALIZE_SOCKET_ENV do { \
+    WSADATA  Ws; \
+    if ( WSAStartup(MAKEWORD(2,2), &Ws) != 0 ) { SOCPERROR("startup"); exit(4); } }while(0)
+
+#define LOG(...) do{ char log_buff[1024]; sprintf_s(log_buff, 1024, __VA_ARGS__); fputs(log_buff, stdout); }while(0)
+#endif
+
+#define DEFAULT_PORT 8888
+#define SOCKET_BACKLOG 24
+#define BUFFER_SIZE 1024
+#define TRUE 1
+#define FALSE 0
+
+
+int main(int, char** );
+void main_loop(HSocket);
+int bind_socket(u_short);
+
+#define HTML "HTTP/1.1 200 OK \n\n\
+<!DOCTYPE html>\n\
+<html>HelloWorld!</html> "
+
+#endif //__SERVER_H__
