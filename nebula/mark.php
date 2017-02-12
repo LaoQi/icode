6c8e971bcf7dea527031ed648b6ffb9e8b62c32e
LaoQi <qjqjqzy@gmail.com>
2017-02-13 02:45:31 +0800

    index.html

--- nebula/mark.php
+++ nebula/mark.php
@@ -1,6 +1,6 @@
-<?php
-
+decode(<?php
 $c = file_get_contents("README.md");
-if (!empty($_GET['callback'])) { echo $_GET['callback'] . '("'; } else { echo 'callback('; }
-echo base64_encode($c);
-echo '");';
+echo '"' . base64_encode($c) . '"';
+?>);
+YouShallNotPass;
+alert("1");
